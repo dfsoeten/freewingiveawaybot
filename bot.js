187e5e6e5fe9aea10482027e419c27456fad4b31
Daan Soeten <d.soeten@gmail.com>
2018-07-16 22:57:46 +0200

    Added Commands

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ const overwatch = require('overwatch-js');
 //Get Auth Key
 const auth = require('./auth.json');
 
+//Define Sorted Ratings
 const ratings = new Map();
 ratings[Symbol.iterator] = function* () {
     yield* [...this.entries()].sort((a, b) => a[1] - b[1]);
@@ -15,13 +16,45 @@ ratings[Symbol.iterator] = function* () {
 
 //Initialize Client & Get Leaderboard
 client.on('ready', () => {
-  var members = client.guilds.get('467356820843790347').members;
-
   //Display Welcome Message
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Bot initialized as ${client.user.tag}!`);
 
-  //Foreach valid member get their SR
-  members.forEach(function(member){
+  getRatings();
+});
+
+client.on('message', message => {
+  //Show The Leaderboard
+  if(message.content === '!leaderboards'){
+    var rank = 1;
+    var result = '__**Leaderboard:**__ \n';
+
+    for(let [player, sr] of ratings){
+      result += '*#' + rank++ + '*   ' + player + '   ' + sr + 'SR \n';
+    }
+
+    message.channel.send(result);
+  }
+
+  //Update Leaderboards
+  if(message.content === '!update-leaderboards'){
+    getRatings();
+    message.channel.send('Leaderboard updated!');
+  }
+
+  //Help
+  if(message.content === '!help'){
+    var result = '__** <-- It\'s Commands:**__ \n';
+    result += '!help - Shows this message \n';
+    result += '!leaderboards - Shows the current leaderboard \n';
+    result += '!update-leaderboards - Refreshes the leaderboard \n';
+
+    message.channel.send(result);
+  }
+});
+
+//Foreach valid member get their SR
+function getRatings(){
+  client.guilds.get('467356820843790347').members.forEach(function(member){
     if(member.nickname && /(.{1,12}#[0-9]{1,10})\w+/g.test(member.nickname)){
       //Get player data
       overwatch.getOverall('pc', 'eu', member.nickname.replace('#', '-'))
@@ -29,8 +62,6 @@ client.on('ready', () => {
             .catch(err => console.log('\x1b[31m', member.nickname + '\'s profile not found', '\x1b[0m'));
     }
   })
-});
-
-
+}
 
 client.login(auth.token);
